ac78eae54a8940d6e0bcbd4687f01387abee0c3c
carlos-garay <carlos_garay1312@outlook.com>
2023-09-28 22:13:08 -0600

    Screen registro alimento

---
+++ insuline_calculator/lib/widgets/dynamic_text_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class DynamicTextField extends StatelessWidget {
+  final TextEditingController myController;
+  final double width;
+  final double height;
+  final dynamic textInputType; 
+  final String label;
+  final int? maxlines;
+  final int? minlines;
+
+  DynamicTextField({super.key, required this.myController, required this.height, required this.width, 
+    required this.textInputType, required this.label, required this.maxlines, required this.minlines});  
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+                  width: width,
+                  height: height,
+                  child: TextField(
+                    controller: myController,
+                    obscureText: false,
+                    keyboardType: textInputType,
+                    minLines: minlines,
+                    maxLines: maxlines,
+                    decoration:  InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)
+                      ),
+                      label:  Center(child: Text(label)),
+                      alignLabelWithHint: true,
+                    ),
+                  ),
+                );
+  }
+}
